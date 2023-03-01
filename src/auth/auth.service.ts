4babdf37b52f07a2187569a90288921b3f5a1eb6
Devdiop221 <diopmouhamed290@gmail.com>
2023-03-01 19:04:58 +0000

    Throw Exceptions errors and update migration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-
-@Injectable({})
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
+@Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService){}
-  signin() {
-    return { msg: 'This is the signin route' };
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    // Hash the password
+    const hash = await argon.hash(dto.password);
+    try {
+      // Save the user in the database
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      // Return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
   }
 
-  signup() {
-    return { msg: 'This is the signup route' };
+  signin() {
+    return { msg: 'This is the signin route' };
   }
 }
